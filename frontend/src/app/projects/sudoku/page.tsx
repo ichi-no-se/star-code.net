923cf5c7388b7e907ad6968a975edea749a3c0b1
1_no_se <46490529+ichi-no-se@users.noreply.github.com>
2025-05-18 21:05:34 +0900

    数独周りをかなり実装

--- frontend/src/app/projects/sudoku/page.tsx
+++ frontend/src/app/projects/sudoku/page.tsx
@@ -1,7 +1,7 @@
-'use client'
-import { useState } from 'react'
-import { SudokuSolver } from '@/lib/sudokuSolver'
-import '@styles/sudoku.css'
+"use client";
+import { useState } from "react";
+import { SudokuSolver } from "@/lib/SudokuSolver";
+import "@styles/sudoku.css";
 
 export default function SudokuPage() {
 	const [board, setBoard] = useState<number[][]>(
@@ -10,45 +10,104 @@ export default function SudokuPage() {
 	const handleChange = (row: number, col: number, value: string) => {
 		const num = parseInt(value);
 		if (isNaN(num) || num < 1 || num > 9) {
-			setBoard(prev => {
+			setBoard((prev) => {
 				const copy = structuredClone(prev);
 				copy[row][col] = 0;
 				return copy;
 			});
-		}
-		else {
-			setBoard(prev => {
+		} else {
+			setBoard((prev) => {
 				const copy = structuredClone(prev);
 				copy[row][col] = num;
-				console.log(copy);
 				return copy;
 			});
 		}
 	};
+	const [solver, setSolver] = useState<SudokuSolver | null>(null);
+	const [result, setResult] = useState<number[][] | null>(null);
 
 	return (
 		<main>
 			<h1 className="title">数独ソルバー</h1>
-			<div className="introduction">
-				高速に動作する数独ソルバーです
-			</div>
+			<div className="introduction">（かなり）高速に動作する数独ソルバー</div>
 			<div className="sudoku-grid">
-				{
-					board.map((row, i) =>
-						row.map((cell, j) => (
-							<input
-								key={`${i}-${j}`}
-								type="text"
-								inputMode="numeric"
-								maxLength={1}
-								className="sudoku-cell"
-								value={cell === 0 ? '' : cell}
-								onChange={(e) => handleChange(i, j, e.target.value)}
-							/>
-						)
-						)
-					)
-				}
+				{board.map((row, i) =>
+					row.map((cell, j) => (
+						<input
+							key={`${i}-${j}`}
+							type="text"
+							inputMode="numeric"
+							maxLength={1}
+							className="sudoku-cell"
+							value={cell === 0 ? "" : cell}
+							onChange={(e) => handleChange(i, j, e.target.value)}
+						/>
+					))
+				)}
+			</div>
+			<div className="sudoku-controls">
+				<button
+					className="sudoku-button"
+					onClick={() => {
+						const s = new SudokuSolver(board);
+						setSolver(s);
+						const firstSolution = s.next();
+						setResult(firstSolution);
+					}}
+				>
+					解く
+				</button>
+				<button
+					className="sudoku-button"
+					onClick={() => {
+						if (!solver?.hasPrev()) {
+							return;
+						}
+						const prev = solver.prev();
+						if (prev) {
+							setResult(prev);
+						}
+					}}
+					disabled={!solver?.hasPrev()}
+				>
+					前へ
+				</button>
+				<button
+					className="sudoku-button"
+					onClick={() => {
+						if (!solver?.hasNext()) {
+							return;
+						}
+						const next = solver.next();
+						if (next) {
+							setResult(next);
+						}
+					}}
+					disabled={!solver?.hasNext()}
+				>
+					次へ
+				</button>
+			</div>
+			<div className="sudoku-result">
+				<h2>現在の解</h2>
+				{solver?.isExhausted() && result === null ? (
+					<p>解が存在しません</p>
+				) : result ? (
+					<div className="sudoku-grid">
+						{result.map((row, i) =>
+							row.map((cell, j) => (
+								<div
+									key={`${i}-${j}`}
+									className={`sudoku-cell ${board[i][j] === 0 ? "solved-cell" : ""}`}
+								>
+									{cell}
+								</div>
+							))
+						)}
+					</div>
+				) : (
+					<p>まだ解が生成されていません</p>
+				)}
 			</div>
 		</main>
 	);
--- frontend/src/lib/SudokuSolver.ts
+++ frontend/src/lib/SudokuSolver.ts
@@ -1,33 +1,153 @@
-export class SudokuSolver{
-	private board: number[][];
+class SolverCore {
+	private isExhausted: boolean = false;
+	private currentBoard: number[] = [];
+	private fillOrder: number[][] = []; // (埋める数字，行の先頭 index)
+	private rowUsed: number[] = []; // 各数字ごとの行利用状況
+	private colUsed: number[] = []; // 各数字ごとの列利用状況
+	private boxUsed: number[] = []; // 各数字ごとのブロック利用状況
+	private cellBoxBit: number[] = []; // それぞれの box の id を 1 << boxID で持つ
+	private history: number[] = []; // スタックの代わりの履歴
+	private numCellsToFill: number = 0;
+	constructor(initialBoard: number[][]) {
+		this.init(initialBoard);
+	}
+	private init(initialBoard: number[][]): void {
+		this.isExhausted = false;
+		this.currentBoard = new Array(81);
+		this.fillOrder = Array.from({ length: 81 }, () => [0, 0]);
+		this.rowUsed = new Array(9).fill(0);
+		this.colUsed = new Array(9).fill(0);
+		this.boxUsed = new Array(9).fill(0);
+		this.cellBoxBit = new Array(81);
+		this.history = new Array(81).fill(-1);
+		this.numCellsToFill = 0;
+
+		const rowAppearCount = new Array(9).fill(0);
+		for (let i = 0; i < 9; i++) {
+			for (let j = 0; j < 9; j++) {
+				const k = i * 9 + j;
+				this.cellBoxBit[k] = 1 << (Math.floor(i / 3) * 3 + Math.floor(j / 3));
+				if (initialBoard[i][j] === 0) {
+					this.currentBoard[k] = -1;
+				} else {
+					const num = initialBoard[i][j] - 1;
+					this.currentBoard[k] = num;
+					if(this.rowUsed[num] & (1 << i) || this.colUsed[num] & (1 << j) || this.boxUsed[num] & this.cellBoxBit[k]) {
+						this.isExhausted = true;
+						return;
+					}
+					this.rowUsed[num] |= 1 << i;
+					this.colUsed[num] |= 1 << j;
+					this.boxUsed[num] |= this.cellBoxBit[k];
+					rowAppearCount[num]++;
+				}
+			}
+		}
+		for (let i = 0; i < 9; i++) {
+			let maxAppearCount = -1;
+			let candidate = -1;
+			for (let j = 0; j < 9; j++) {
+				if (rowAppearCount[j] > maxAppearCount) {
+					maxAppearCount = rowAppearCount[j];
+					candidate = j;
+				}
+			}
+			rowAppearCount[candidate] = -1;
+			for (let j = 0; j < 9; j++) {
+				if (~this.rowUsed[candidate] & (1 << j)) {
+					this.fillOrder[this.numCellsToFill][0] = candidate;
+					this.fillOrder[this.numCellsToFill][1] = j * 9; // 行の先頭 index
+					this.numCellsToFill++;
+				}
+			}
+		}
+	}
+
+	public findNextSolution(isInitial: boolean = false): number[][] | null {
+		if (this.isExhausted) {
+			return null;
+		}
+		let currentIndex;
+		if (isInitial) {
+			currentIndex = 0;
+		} else {
+			currentIndex = this.numCellsToFill - 1;
+		}
+		while (currentIndex !== this.numCellsToFill && currentIndex !== -1) {
+			const num = this.fillOrder[currentIndex][0];
+			const rowIndex = this.fillOrder[currentIndex][1];
+			if (this.history[currentIndex] !== -1) {
+				this.colUsed[num] &= ~(1 << this.history[currentIndex]);
+				this.boxUsed[num] &= ~this.cellBoxBit[rowIndex + this.history[currentIndex]];
+				this.currentBoard[rowIndex + this.history[currentIndex]] = -1;
+			}
+			while (true) {
+				this.history[currentIndex]++;
+				if (this.history[currentIndex] === 9) {
+					this.history[currentIndex] = -1;
+					currentIndex--;
+					break;
+				}
+				if (~this.colUsed[num] & (1 << this.history[currentIndex]) && ~this.boxUsed[num] & this.cellBoxBit[rowIndex + this.history[currentIndex]] && this.currentBoard[rowIndex + this.history[currentIndex]] === -1) {
+					this.colUsed[num] |= 1 << this.history[currentIndex];
+					this.boxUsed[num] |= this.cellBoxBit[rowIndex + this.history[currentIndex]];
+					this.currentBoard[rowIndex + this.history[currentIndex]] = num;
+					currentIndex++;
+					break;
+				}
+			}
+		}
+		if (currentIndex === this.numCellsToFill) {
+			const solution = Array.from({ length: 9 }, () => Array(9));
+			for (let i = 0; i < 9; i++) {
+				for (let j = 0; j < 9; j++) {
+					solution[i][j] = this.currentBoard[i * 9 + j] + 1;
+				}
+			}
+			return solution;
+		}
+		else {
+			this.isExhausted = true;
+			return null;
+		}
+	}
+	public getIsExhausted(): boolean {
+		return this.isExhausted;
+	}
+}
+
+export class SudokuSolver {
+	private core: SolverCore;
 	private solutions: number[][][] = [];
 	private currentIndex: number = -1;
-	private currentState: number[][] = [];
 
 	constructor(board: number[][]) {
-		this.board = structuredClone(board)
-		this.currentState = structuredClone(board);
+		this.core = new SolverCore(board);
 		this.prefetchNextSolution(true);
 	}
 
 	private prefetchNextSolution(isInitial: boolean = false): void {
-		
+		const solution = this.core.findNextSolution(isInitial);
+		if (solution) {
+			this.solutions.push(solution);
+		}
 	}
 
-	public hasNext(): boolean{
-		return this.currentIndex < this.solutions.length - 1 && this.solutions.length > 0;
+	public hasNext(): boolean {
+		return (
+			this.currentIndex < this.solutions.length - 1 && this.solutions.length > 0
+		);
 	}
 
-	public hasPrev():boolean{
+	public hasPrev(): boolean {
 		return this.currentIndex > 0;
 	}
 
-	public next(): number[][] | null{
-		if(this.currentIndex < this.solutions.length - 1){
+	public next(): number[][] | null {
+		if (this.currentIndex < this.solutions.length - 2) {
 			this.currentIndex++;
 			return this.solutions[this.currentIndex];
-		}
-		else {
+		} else {
 			this.prefetchNextSolution();
 			if (this.solutions.length > this.currentIndex + 1) {
 				this.currentIndex++;
@@ -37,18 +157,22 @@ export class SudokuSolver{
 		return null;
 	}
 
-	public prev(): number[][] | null{
-		if(this.currentIndex > 0){
+	public prev(): number[][] | null {
+		if (this.currentIndex > 0) {
 			this.currentIndex--;
 			return this.solutions[this.currentIndex];
 		}
 		return null;
 	}
 
-	public current(): number[][]| null{
+	public current(): number[][] | null {
 		if (this.currentIndex >= 0 && this.currentIndex < this.solutions.length) {
 			return this.solutions[this.currentIndex];
 		}
 		return null;
+	}
+
+	public isExhausted(): boolean {
+		return this.core.getIsExhausted();
 	}
 }
