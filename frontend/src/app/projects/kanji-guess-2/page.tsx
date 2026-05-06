c5685f16a1b47a3512ae5c3dbee484d96a9a9db2
Hoshii Tomohito <tomo.hoshii47@gmail.com>
2026-05-06 13:35:06 +0900

    kanji-guess 2 を作り出した

---
+++ frontend/src/app/projects/kanji-guess-2/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import Link from "next/link";
+import KanjiGuess from "@/components/KanjiGuess";
+import "@styles/kanji-guess.css";
+
+export default function KanjiGuess2Page() {
+	return (
+		<>
+			<h1 className="title">fastText 類似度漢字当てゲーム 2</h1>
+			<div className="introduction">
+			</div>
+		</>
+	)
+}
